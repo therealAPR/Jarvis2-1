353ee2da5947c78c37573cf79b8da454245249cf
prajaydevsys <64075011+prajaydevsys@users.noreply.github.com>
2020-05-05 20:24:00 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,9 +13,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+    await alive.edit("`Jinda Hu Sarr aur wo bsdk kaun hai ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.4\n\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @therealApr\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.4\nfork by:` @therealApr\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      "[Deploy this userbot Now](https://github.com/prajaydevsys/Jarvis2)\n\n"
                     "[Bot Creator](https://t.me/therealApr)") 
